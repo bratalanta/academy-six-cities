7d8b81256f08e7584b6d8559539e2109ed41a066
Matvei <mateodenisous69@gmail.com>
2022-07-25 01:07:22 +0300

    Создаст редюсер и действия

---
+++ project/src/store/action.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const setCity = createAction('main/changeCity');
+export const setProperties = createAction('main/fillProperties');
---
+++ project/src/store/reducer.ts
@@ -0,0 +1,27 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { properties } from '../mocks/properties';
+import { Properties, PropertyCity } from '../types/property';
+import { setProperties } from './action';
+
+type InititalState = {
+  city: PropertyCity
+  properties: Properties;
+}
+
+const initialState: InititalState = {
+  city: {
+    name: 'Paris',
+    location: {
+      latitude: 48.85661,
+      longitude: 2.351499,
+      zoom: 10
+    }},
+  properties
+};
+
+export const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(setProperties, (state, {payload}) => {
+      state.properties = payload.properties;
+    });
+});
