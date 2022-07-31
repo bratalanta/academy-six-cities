9b398910cbac16df45a8a4e91b47f77f52e20318
Matvei <mateodenisous69@gmail.com>
2022-07-31 14:40:34 +0300

    Добавит перехват при запросе и добавит туда токен

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,11 +1,24 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://10.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
-export const createAPI = (): AxiosInstance => (
-  axios.create({
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT
-  })
-);
+  });
+
+  axios.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
+  });
+
+  return api;
+};
